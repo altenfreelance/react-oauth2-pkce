d6fdf5b2e74a43002e8e5c8eb8452df286155fa5
Andrew Alten <altenfreelance@gmail.com>
2021-09-01 17:18:59 -0400

    Trying again

--- src/pkce.ts
+++ src/pkce.ts
@@ -1,6 +1,9 @@
 import sha256 from 'crypto-js/sha256';
 const CryptoJS = require("crypto-js");
 
+import b64id from 'b64id';
+
+
 
 export type PKCECodePair = {
   codeVerifier: string
@@ -8,20 +11,10 @@ export type PKCECodePair = {
   createdAt: Date
 }
 
-const generateRandomString = (): string => {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < 64; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return CryptoJS.enc.Utf8.parse(text);
-}
-
 
 export const createPKCECodes = (): PKCECodePair => {
-  const codeVerifier = btoa(generateRandomString())
-  const codeChallenge = btoa(sha256(codeVerifier).toString(CryptoJS.enc.Base64))
+  const codeVerifier = b64id.uuidToB64(b64id.generateId())
+  const codeChallenge = b64id.uuidToB64(sha256(codeVerifier).toString(CryptoJS.enc.Base64))
   const createdAt = new Date()
   const codePair = {
     codeVerifier,
