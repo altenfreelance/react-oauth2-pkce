d6b1a7dc75816342810da5e8750d3a02c57a3592
Andrew Alten <altenfreelance@gmail.com>
2021-09-01 17:03:39 -0400

    b64 encode url error bug fix

--- src/pkce.ts
+++ src/pkce.ts
@@ -1,5 +1,6 @@
 import sha256 from 'crypto-js/sha256';
 const CryptoJS = require("crypto-js");
+import { encode } from 'url-safe-base64'
 
 
 export type PKCECodePair = {
@@ -20,8 +21,9 @@ const generateRandomString = (): string => {
 
 
 export const createPKCECodes = (): PKCECodePair => {
-  const codeVerifier = btoa(generateRandomString())
-  const codeChallenge = btoa(sha256(codeVerifier).toString(CryptoJS.enc.Base64))
+
+  const codeVerifier = encode(generateRandomString())
+  const codeChallenge = encode(sha256(codeVerifier).toString(CryptoJS.enc.Base64))
   const createdAt = new Date()
   const codePair = {
     codeVerifier,
